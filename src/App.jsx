f1425d10cd607da6f83238811c9e67b258be4d2b
aitorz09 <pqpiacarreras@gmail.com>
2024-05-26 17:14:15 +0200

    remove browserrotuer

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 
 import './App.css'
 import { MainLayout } from './layout/MainLayout.jsx'
-import { Route, Router, Routes } from 'react-router-dom'
 import { Main } from './components/Main/Main.jsx'
 
 
 function App() {
 
   return (
-  
     <MainLayout>
-      <Routes>
-        <Route path='/' element={<Main/>}/>
-      </Routes>
+      <Main/>
     </MainLayout>
     
   )
